f08d0e12bb948a27859f232432a64e3873eea0ca
Tokhir2311 <toxirjuraboyev23@gmail.com>
2026-02-07 13:26:33 +0000

    next

--- app/models.py
+++ app/models.py
@@ -68,17 +68,20 @@ class Post(BaseModel):
 class Users(BaseModel):
     __tablename__="users"
 
-    profession_id :Mapped[int] = mapped_column(BigInteger, ForeignKey("profession.id"))
+    profession_id :Mapped[int] = mapped_column(BigInteger, ForeignKey("profession.id"), nullable = True)
     email: Mapped[str] = mapped_column(String, unique=True, nullable=False)
     password_hash: Mapped[str] = mapped_column(String(120), nullable=False)
-    name : Mapped[str] = mapped_column(String(45), nullable=False)
-    surname: Mapped[str] = mapped_column(String(45))
-    bio: Mapped[str] = mapped_column(Text)
+    name : Mapped[str] = mapped_column(String(45), nullable=True)
+    surname: Mapped[str] = mapped_column(String(45), nullable=True)
+    bio: Mapped[str] = mapped_column(Text, nullable=True)
     post_cnt: Mapped[int] = mapped_column(BigInteger, default=0)
     post_view_cnt: Mapped[int] = mapped_column(BigInteger, default=0)
-    is_active: Mapped[bool] = mapped_column(Boolean, nullable=True)
-    is_staff: Mapped[bool] = mapped_column(Boolean, nullable=True)
-    is_superuser: Mapped[bool] = mapped_column(Boolean, nullable=True)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    is_staff: Mapped[bool] = mapped_column(Boolean, default=False)
+    is_superuser: Mapped[bool] = mapped_column(Boolean, default=False)
+    deleted_email: Mapped[str] = mapped_column(String(60), nullable=True)
+    is_deleted: Mapped[bool] = mapped_column(Boolean, default=False, nullable=True)
+
 
     professions: Mapped["Profession"] = relationship("Profession", back_populates="users")
     comments: Mapped[list["Comment"]] = relationship("Comment", back_populates="user")
